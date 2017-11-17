01d056f602fd61f2ad885614e4ffac9887eed0cb
zhuangyuantao <962810652@qq.com>
2017-11-17 18:14:09 +0800

    首页推荐专题列表和批量首页推荐jsp

--- yuxin-school-web/src/main/java/com/yuxin/wx/controller/classes/appNewClasses/shelvesCourses.java
+++ yuxin-school-web/src/main/java/com/yuxin/wx/controller/classes/appNewClasses/shelvesCourses.java
@@ -27,6 +27,27 @@ public class  shelvesCourses {
     public String gotoInformationEditing(){
         return "simpleClasses/appNewClasses/informationEditing";
     }
+    /**
+     * 跳转到首页推荐专题列表
+     */
+    @RequestMapping(value="/recommendedList")
+    public String gotorecommendedList(){
+        return "simpleClasses/appNewClasses/recommendedList";
+    }
+    /**
+     * 跳转到批量首页推荐
+     */
+    @RequestMapping(value="/pageRecommendation")
+    public String gotopageRecommendation(){
+        return "simpleClasses/appNewClasses/pageRecommendation";
+    }
+    /**
+     * 跳转到推荐列表
+     */
+    @RequestMapping(value="/recommendSpecialList")
+    public String gotorecommendSpecialList(){
+        return "simpleClasses/appNewClasses/recommendSpecialList";
+    }
 
 
 
