37210191d4255aba3725fa3cb2cc061bff6bc39c
linyuanmao <1712309544@qq.com>
2017-11-30 00:56:58 +0800

    前台通知接口开发

--- yuxin-school-model/src/main/java/com/yuxin/wx/vo/user/UserMessageVo.java
+++ yuxin-school-model/src/main/java/com/yuxin/wx/vo/user/UserMessageVo.java
@@ -7,8 +7,83 @@ public class UserMessageVo extends BaseEntity {
 
 	private Integer	userId;		 /* 用户表id */ 
 	private Integer	messageId;		 /* 学员通知表id */ 
-	private Integer	readFlag;		 /* 阅读标记（1：已读；0：未读） */ 
-	
+	private Integer	readFlag;		 /* 阅读标记（1：已读；0：未读） */
+	private String title;   /*标题*/
+	private String msgType; /*消息类型*/
+	private String content; /*消息内容*/
+
+	private String creatTime;/*发送时间*/
+	private String imgUrl;/*封面图片*/
+	private String h5Url;
+	private String contentText;/*通知纯文本*/
+	private String erroInfo;
+
+	public String getErroInfo() {
+		return erroInfo;
+	}
+
+	public void setErroInfo(String erroInfo) {
+		this.erroInfo = erroInfo;
+	}
+
+	public String getContentText() {
+		return contentText;
+	}
+
+	public void setContentText(String contentText) {
+		this.contentText = contentText;
+	}
+
+	public String getH5Url() {
+		return h5Url;
+	}
+
+	public void setH5Url(String h5Url) {
+		this.h5Url = h5Url;
+	}
+
+	public String getCreatTime() {
+		return creatTime;
+	}
+
+	public void setCreatTime(String creatTime) {
+		this.creatTime = creatTime;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getMsgType() {
+		return msgType;
+	}
+
+	public void setMsgType(String msgType) {
+		this.msgType = msgType;
+	}
+
+
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
 	private CompanyStudentMessageVo message;	/* 信息标题*/
 
 	public Integer getUserId() {
