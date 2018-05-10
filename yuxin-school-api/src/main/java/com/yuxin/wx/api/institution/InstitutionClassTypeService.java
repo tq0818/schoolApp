02bd32ebf6cc69d705e3df4955e6601353781228
tanqiang <364049618@qq.com>
2018-05-10 14:00:38 +0800

    课程相关功能

--- yuxin-school-api/src/main/java/com/yuxin/wx/api/institution/InstitutionClassTypeService.java
+++ yuxin-school-api/src/main/java/com/yuxin/wx/api/institution/InstitutionClassTypeService.java
@@ -6,14 +6,44 @@ import com.yuxin.wx.common.PageFinder;
 import com.yuxin.wx.model.institution.InstitutionClassTypeVo;
 
 public interface InstitutionClassTypeService {
+	 /**
+	  * 新建课程
+	  * @param entity
+	  */
 	 void insert(InstitutionClassTypeVo entity);
+	 /**
+	  * 批量新建课程
+	  * @param list
+	  */
 	 void batchInsert(List<InstitutionClassTypeVo> list);
+	 /**
+	  * 根据课程Id删除课程
+	  * @param id
+	  */
 	 void deleteById(Integer id);
+	 /**
+	  * 根据课程ID批量删除课程
+	  * @param ids
+	  */
 	 void deleteByIds(List<Integer> ids);
+	 /**
+	  * 更新课程
+	  * @param entity
+	  */
 	 void update(InstitutionClassTypeVo entity);
+	 /**
+	  * 根据课程id获取课程
+	  * @param id
+	  * @return
+	  */
 	 InstitutionClassTypeVo findById(Integer id);
+	 /**
+	  * 获取所有课程
+	  * @return
+	  */
 	 List<InstitutionClassTypeVo> queryAll();
 	
-	 PageFinder<InstitutionClassTypeVo> page(Integer status,int pageStart,int pageSize);
+	
+	 PageFinder<InstitutionClassTypeVo> page(Integer insId,Integer status,int pageStart,int pageSize);
 	
 }
--- yuxin-school-web/src/main/java/com/yuxin/wx/controller/institution/InstitutionClassTypeController.java
+++ yuxin-school-web/src/main/java/com/yuxin/wx/controller/institution/InstitutionClassTypeController.java
@@ -5,13 +5,16 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yuxin.wx.api.institution.InstitutionClassTypeService;
+import com.yuxin.wx.api.institution.InstitutionInfoService;
 import com.yuxin.wx.common.PageFinder;
 import com.yuxin.wx.model.institution.InstitutionClassTypeVo;
+import com.yuxin.wx.model.institution.InstitutionInfoVo;
 
 @Controller
 @RequestMapping("/institutionClassType")
@@ -21,12 +24,41 @@ public class InstitutionClassTypeController {
 	@Autowired
 	private InstitutionClassTypeService institutionClassTypeService;
 
+	@Autowired
+	private InstitutionInfoService institutionInfoService;
+	
+	/**
+	 * 进入课程管理首页
+	 * @param model
+	 * @param insId
+	 * @return
+	 */
+	 @RequestMapping(value = "/classTypeMain/{insId}")
+	 public String intoClassTypeMain(Model model,@PathVariable Integer insId){
+	      //  PageFinder<InstitutionInfoVo> pageFinder = institutionInfoService.findInstitutionInfos(insInfoVo);
+	      //  model.addAttribute("pageFinder",pageFinder);
+	     //   return "institution/organizationIndex";
+		 
+		 InstitutionInfoVo insEntity =  institutionInfoService.findInstitutionInfoById(insId);
+		 model.addAttribute("insEntity",insEntity);
+		 return "institution/course";
+		 
+	}
+	
+	 /**
+	  * 获取课程分页列表信息
+	  * @param insId	机构ID
+	  * @param status	课程状态
+	  * @param pageStart	分页开始位置
+	  * @param pageSize	分页大小
+	  * @return
+	  */
 	@ResponseBody
 	@RequestMapping(value = "/getClassTypeList", method = RequestMethod.POST)
-	public PageFinder<InstitutionClassTypeVo> findInstitutionCategoryById(Integer status, int pageStart, int pageSize) {
+	public PageFinder<InstitutionClassTypeVo> findInstitutionCategoryById(Integer insId,Integer status, int pageStart, int pageSize) {
 		// PageFinder<InstitutionClassTypeVo>
 		// return institutionCategoryService.findInstitutionCategoryById(id);
-		return institutionClassTypeService.page(status, pageStart, pageSize);
+		return institutionClassTypeService.page(insId,status, pageStart, pageSize);
 	}
 	
 	
